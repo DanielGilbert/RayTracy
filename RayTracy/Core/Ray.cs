aaf5adb568adbddeb24d0efeda8198de1d25db66
Daniel Gilbert <ich@danielgilbert.de>
2022-02-18 21:16:13 +0100

    Adds a simple ray class

---
+++ RayTracy/Core/Ray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayTracy.Core
+{
+    public class Ray
+    {
+        public Vector3 Origin { get; }
+        public Vector3 Direction { get; }
+
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+
+        public Vector3 At(float t)
+        {
+            return Origin + t * Direction;
+        }
+    }
+}
