64a1538fa59e24f6e740fc7bf9106b15013f4438
Daniel Gilbert <ich@danielgilbert.de>
2022-02-18 20:51:35 +0100

    Adds ImageSharp for writing images

--- RayTracy/Program.cs
+++ RayTracy/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+int width = 640;
+int height = 480;
+
+using (Image<Rgba32> image = new(width, height))
+{
+    Console.WriteLine("Start writing image..");
+    for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
+        {
+            image[x, y] = new Rgba32(0, 255, 255, 255);
+        }
+
+    image.SaveAsBmp("test.bmp");
+    Console.WriteLine("Image saved.");
+}
