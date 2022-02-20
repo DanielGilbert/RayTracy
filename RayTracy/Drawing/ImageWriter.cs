b085d7e45da88222d4e295f2ecf45b8d71df9890
Daniel Gilbert <ich@danielgilbert.de>
2022-02-20 12:29:57 +0100

    Make output look like reference code

--- RayTracy/Drawing/ImageWriter.cs
+++ RayTracy/Drawing/ImageWriter.cs
@@ -12,11 +12,23 @@ namespace RayTracy.Drawing
 {
     public class ImageWriter
     {
+        public void TestImage(string path, int imageHeight, int imageWidth)
+        {
+            using (Image<Rgba32> image = new(imageWidth, imageHeight))
+            {
+                image[0, imageHeight - 1] = new Rgba32(255, 0, 0);
+                image[imageWidth - 1, 0] = new Rgba32(0, 255, 0);
+                image[imageWidth - 1, imageHeight - 1] = new Rgba32(0, 0, 255);
+
+                image.SaveAsBmp(path);
+            }
+        }
+
         public void WriteImage(string path, int imageHeight, int imageWidth, Rgba32[,] data)
         {
             using (Image<Rgba32> image = new(imageWidth, imageHeight))
             {
-                for (int y = imageHeight - 1; y >= 0; y--)
+                for (int y = 0; y < imageHeight; y++)
                     for (int x = 0; x < imageWidth; x++)
                     {
                         image[x, y] = data[x,y];
@@ -30,8 +42,8 @@ namespace RayTracy.Drawing
         {
             using (Image<Rgba32> image = new(imageWidth, imageHeight))
             {
-                for (int x = 0; x < imageWidth; x++)
-                    for (int y = 0; y < imageHeight; y++)
+                for (int y = 0; y < imageHeight; y++)
+                    for (int x = 0; x < imageWidth; x++)
                     {
                         image[x, y] = Rgba32Factory.FromNormalizedVector3(data[x, y]);
                     }
--- RayTracy/Program.cs
+++ RayTracy/Program.cs
@@ -11,14 +11,15 @@ ImageWriter imageWriter = new();
 
 Rgba32[,] data = new Rgba32[width, height];
 
-for(int y = height - 1; y >= 0 ; y--)
+for(int y = 0; y < height ; y++)
     for(int x = 0; x < width; x++)
     {
         float r = (float)x / (float)(width - 1);
-        float g = (float)y / (float)(height - 1);
+        float g = 1.0f - ((float)y / (float)(height - 1));
         float b = 0.25f;
 
         data[x, y] = Rgba32Factory.FromNormalizedComponents(r, g, b);
     }
 
 imageWriter.WriteImage("test.bmp", 480, 640, data);
+imageWriter.TestImage("test2.bmp", 480, 640);
